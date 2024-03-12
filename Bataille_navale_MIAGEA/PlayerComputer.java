64a685f9a3bac5f1f86e054216f887a636c0a1d3
AbdBoutchichi <98016219+AbdBoutchichi@users.noreply.github.com>
2024-03-12 09:56:12 +0100

    Add files via upload

---
+++ Bataille_navale_MIAGEA/PlayerComputer.java
@@ -0,0 +1,58 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class PlayerComputer extends Player {
+
+    /**
+     * Default constructor
+     */
+    public PlayerComputer() {
+    }
+
+    /**
+     * 
+     */
+    public int difficulte;
+
+    /**
+     * 
+     */
+    public int currentBateau;
+
+    /**
+     * @param enemie 
+     * @return
+     */
+    public void shoot(Player enemie) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public boolean isCible() {
+        // TODO implement here
+        return false;
+    }
+
+    /**
+     * @return
+     */
+    public void analyserTir() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void setDifficulte() {
+        // TODO implement here
+        return null;
+    }
+
+}
---
+++ Bataille_navale_MIAGEA/SaveGamePart.java
@@ -0,0 +1,65 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class SaveGamePart {
+
+    /**
+     * Default constructor
+     */
+    public SaveGamePart() {
+    }
+
+    /**
+     * 
+     */
+    public type field;
+
+    /**
+     * 
+     */
+    public void Attribute1;
+
+    /**
+     * @return
+     */
+    public void createFile() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void updateFiles() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void loadFile() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void deleteFile() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void deleteAllFiles() {
+        // TODO implement here
+        return null;
+    }
+
+}
