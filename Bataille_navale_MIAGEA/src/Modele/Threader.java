d5a5a008ef16b04cbc33ab8b92898422e9fb07ab
LilianMJ <lilianmj971@gmail.com>
2024-04-08 15:38:40 +0200

    Ajout des Thread pour le mode Artillerie

---
+++ Bataille_navale_MIAGEA/src/Modele/Threader.java
@@ -0,0 +1,45 @@
+package Modele;
+
+import java.util.Scanner;
+
+public class Threader {
+
+    int x;
+    Scanner scanner;
+
+    boolean lifeOfThread;
+
+    Thread counterX = new Thread(() -> {
+        try {
+            System.out.println("Le compteur se lance");
+            while (true) {
+             // Défilement automatique jusqu'à ce que le thread soit interrompu
+                System.out.println("" + x);
+                Thread.sleep(100); // Défilement toutes les 1 seconde
+                x++; // Incrémentation de la valeur de la coordonnée
+                if(x==10) x=0;
+            }
+            
+        } catch (InterruptedException e) {
+            System.err.println("X vaut :" + x);
+            
+        }
+        System.out.println ("Fin du décompte X");
+    });
+
+
+    public Threader(){
+        x=0;
+        scanner = new Scanner(System.in);
+        
+    }
+
+    public int roue(){
+        counterX.start();
+        scanner.nextLine();
+        counterX.interrupt();
+        return x;
+    }
+
+
+}
