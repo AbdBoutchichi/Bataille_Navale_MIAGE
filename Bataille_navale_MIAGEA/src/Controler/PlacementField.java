5687686e5a4424243cd23adcc625efc4cf77df16
LilianMJ <lilianmj971@gmail.com>
2024-05-13 11:19:33 +0200

    Modification PlacementField

--- Bataille_navale_MIAGEA/src/Controler/PlacementField.java
+++ Bataille_navale_MIAGEA/src/Controler/PlacementField.java
@@ -7,7 +7,8 @@ import javax.swing.JTextField;
 import Modele.Boat;
 import Modele.Player;
 import View.GridPanel;
-import View.PlacementPage;
+//import View.PlacementPage;
+import View.PlacementPanel;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,31 +23,31 @@ public class PlacementField  implements ActionListener{
     private String name;
     private int dimension;
     private GridPanel grille;
-    private PlacementPage page;
+    private PlacementPanel page;
     private boolean random;
 
-    public PlacementField(Player player, GridPanel placement, JTextField posX, JTextField posY, String orientation, PlacementPage p){
+    public PlacementField(Player player, GridPanel placement, JTextField posX, JTextField posY, String orientation, PlacementPanel placementPanel){
         
         this.player = player;
         this.x = posX;
         this.y = posY;
-        this.size = p.selectedSize;
-        this.name = p.selectedBoat;
+        this.size = placementPanel.selectedSize;
+        this.name = placementPanel.selectedBoat;
         this.grille = placement;
-        this.page = p;
+        this.page = placementPanel;
         this.orientation = orientation;
     }
 
     public void actionPerformed(ActionEvent e){
         
-        Boat b = player.recupBoat(name);
-        player.removeBoat(name);
-        if(player.canPlace(x.getText(), y.getText(), size, orientation.charAt(0))){
+        Boat b = player.recupBoat(page.selectedBoat);
+        player.removeBoat(page.selectedBoat);
+        if(player.canPlace(x.getText(), y.getText(), page.selectedSize, ((String) page.orientationComboBox.getSelectedItem()).charAt(0))){
             System.out.println("les Field valent exactement " + x.getText() + " et " + y.getText());
             grille.removeAll();
             
-            player.placeBateau(x.getText(), y.getText(), orientation.charAt(0), size, name);
-            grille.initGridPanelPlacement(player, orientation, size, name);
+            player.placeBateau(x.getText(), y.getText(), ((String) page.orientationComboBox.getSelectedItem()).charAt(0), page.selectedSize , page.selectedBoat);
+            grille.initGridPanelPlacement(player, orientation, page.selectedSize, page.selectedBoat);
 
             y.repaint();
             y.revalidate();
